b6bc0e12a4493902bba92fdc4b59bb1758764c54
sanderhs <sanderhenrique@visaojr.com.br>
2018-02-06 19:17:06 -0200

    imagem

--- SistemaOutsideFood/adicionarprato.php
+++ SistemaOutsideFood/adicionarprato.php
@@ -27,6 +27,20 @@ require_once 'headercliente.php';
                                     </div>
                                 </div>
                                 
+
+                                <div class="form-group">
+                                  <label class="col-md-4 control-label">Treinamentos</label>
+                                  <div class="col-md-8"> 
+                                    
+                                      <select id='pre-selected-options' multiple='multiple' id="treinamentos" name="">
+                                    <option value=""></option>;
+                                  
+                                  </select>
+                                   <p></p>                                  
+                                  </div>
+                                </div>   
+
+
                                 <div class="row">    
                                     <div class="col-xs-12 col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
                                         <label>Foto</label>
